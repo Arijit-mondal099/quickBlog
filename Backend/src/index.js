914082799b61eeab0a4c98fae6f2f37d0bf4269e
Arijit Mondal <arijitm717@gmail.com>
2025-07-25 21:27:25 +0530

    update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,6 +10,7 @@ await dbConnection(); // data base connection
 // ========== Middlewares ==========
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
+
 app.use(
   cors({
     origin: [
