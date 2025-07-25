110ab55b881b07ebebb86e025cc2298ec6d53795
Arijit Mondal <arijitm717@gmail.com>
2025-07-25 20:43:51 +0530

    cors update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ await dbConnection(); // data base connection
 // ========== Middlewares ==========
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true, }));
+app.use(cors({ origin: "https://quick-blog-nine-kappa.vercel.app/", credentials: true, }));
 
 // ========== Routes ===============
 import adminRouter from "./routes/admin.routes.js";
