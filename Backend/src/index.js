a6dbb5e05169c1bdbac3b01dbc6e47a33db4e1d5
Arijit Mondal <arijitm717@gmail.com>
2025-07-25 20:47:59 +0530

    update

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ await dbConnection(); // data base connection
 // ========== Middlewares ==========
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ limit: "16kb" }));
-app.use(cors({ origin: "https://quick-blog-nine-kappa.vercel.app/", credentials: true, }));
+app.use(cors({ origin: "https://quick-blog-nine-kappa.vercel.app", credentials: true, }));
 
 // ========== Routes ===============
 import adminRouter from "./routes/admin.routes.js";
